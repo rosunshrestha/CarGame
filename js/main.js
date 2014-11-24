8982ba44c8778463c76073a84cfb47bb13253ec6
roshan <rosun.shrestha@gmail.com>
2014-11-24 13:10:22 +0545

    Coding readability and menu updates

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-﻿aa:
-var currentScore=0;
+﻿var currentScore=0;
 var currentLevel=1;
 var level=document.getElementById('Level');
 var parent=document.getElementById('mainroad');
 var scoreBoard=document.getElementById('Scorecard');
 var levelCheck=0;
-counter = 0;
-opponentlist = [];
+var counter = 0;
+var opponentlist = [];
 var universalGap=5000;
 var gap=5000;
 var test=500;
 var move=0;
 var e = setInterval(game,gap);
-//game();
 var pos=1;
+var consts = {}; // Object to store all constants
+consts.left= 37;                   
+consts.top= 38;
+consts.right= 39;
+consts.bottom= 40;
+consts.vanishPoint=575;
+consts.lane1=0;
+consts.lane2=1;
+consts.lane3=2;
+consts.lane1Position=0;
+consts.lane2Position=110;
+consts.lane3Position=220;
+Object.freeze(consts);
 
 function game() {
 	if(counter==0)
@@ -23,20 +34,11 @@ function game() {
 		player.create();
 	}
 	var g = new Gameloop();
-   // if (counter >= 5)
-    //{
-      //  counter = 0;
-   g.makeopponent();
-    //counter++;
+   	var we=g.makeopponent();
+    opponentlist.push(we);
 	clearInterval(e);
-    
 	e = setInterval(game,gap);
-
 }
-
-
-
-
 
 
 function Gameloop() {
@@ -48,92 +50,81 @@ function Gameloop() {
 	this.levelelement;
 	this.position;
 	var that = this;
-	
-    this.makeopponent = function() {
+	this.makeopponent = function() {
 
         that.element = document.createElement('div');
         var oppo=getRandomArbitrary(0, 3);
 		if(oppo==1)
 		{
-		that.element.className = 'opponent2';
+			that.element.className = 'opponent2';
 		}
 		else if(oppo==2)
 		{
-		that.element.className = 'opponent3';
+			that.element.className = 'opponent3';
 		}
 		else
 		{
-		that.element.className = 'opponent4';
+			that.element.className = 'opponent4';
 		}
         parent.appendChild(that.element);
         that.x = getRandomArbitrary(0, 3);
         if (that.x == 0)
-            {that.x = 0;
-			that.position=0;}
+        {	
+			that.x = consts.lane1Position;
+			that.position=consts.lane1;
+		}
         else if (that.x == 1) {
-            that.x = 110;
-			that.position=1;
-        } else if (that.x == 2) {
-            that.x = 220;
-			that.position=2;
+            that.x = consts.lane2Position;
+			that.position=consts.lane2;
+        } 
+		else if (that.x == 2) {
+            that.x = consts.lane3Position;
+			that.position=consts.lane3;
         }
 		that.y = 0;
-
-       
-        that.element.style.top = that.y + "px";
+		that.element.style.top = that.y + "px";
         that.element.style.left = that.x + "px";
         that.element.style.backgroundColor = "yellogreen";
-		
-		
-        that.intervalId = setInterval(that.moveopponent,20);
+		that.intervalId = setInterval(that.moveopponent,20);
     };
     this.moveopponent = function () {
         that.y+=currentLevel*2;
 		if((that.y>=(test-108||that.y<=(test))) && that.position==pos ){
-			
 			parent.style.backgroundImage="url(images/gameover.png)";
 			parent.style.backgroundPosition="0px 0px";
 			parent.style.backgroundRepeat="none";
-            parent.removeChild(that.element);
-			
-
+			clearInterval(that.intervalId);
 			while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-	
-	
-}
-	exitGame();
+    			parent.removeChild(parent.firstChild);
+				move=0;
+			}
+			exitGame();
 		}
         that.element.style.top = that.y + "px";
 		move=move+1;
 		parent.style.backgroundPosition="0px"+" "+move+"px";
-        if (that.y >575) {
+        if (that.y > consts.vanishPoint) {
             clearInterval(that.intervalId);
-            parent.removeChild(that.element);
-        
+          	parent.removeChild(that.element);
 			that.scoreelement=that.score();
-			}
-		
-
-    }
+		}
+	}
 	
 	this.score=function(){
 		that.element = document.createElement('span');
-//        that.element.className = 'opponent';
 		while(scoreBoard.firstChild){
-			
-		scoreBoard.removeChild(scoreBoard.firstChild);}
-        currentScore+=currentLevel*10;
-		scoreBoard.appendChild(that.element);
-		that.element.innerHTML="Score:"+currentScore;
-		that.element.style.top=10+"px";
-		that.element.style.left=10+"px";
-		if(currentScore>=100*Math.pow(currentLevel,2))
-		boostLevel();
-	}
+			scoreBoard.removeChild(scoreBoard.firstChild);}
+        	currentScore+=currentLevel*10;
+			scoreBoard.appendChild(that.element);
+			that.element.innerHTML="Score:"+currentScore;
+			that.element.style.top=10+"px";
+			that.element.style.left=10+"px";
+			if(currentScore>=100*Math.pow(currentLevel,2))
+				boostLevel();
+		}
 	
 	function boostLevel()
-{
+	{
 		element=document.createElement('span');
 		while(level.firstChild)
 		{
@@ -145,94 +136,84 @@ function Gameloop() {
 		element.innerHTML="Level:"+currentLevel;
 		element.style.top=10+"px";
 		element.style.left=10+"px";
+	}
 		
+	function getRandomArbitrary(min, max) {
+		return parseInt(Math.random() * (max - min) + min).toFixed(0);
 	}
 	
-
-    function getRandomArbitrary(min, max) {
-        return parseInt(Math.random() * (max - min) + min).toFixed(0);
-
-    }
-
-}
-
-
-function Player() {
-    this.x = 0;
-    this.y = 0;
-    this.element;
-	var that = this;
+	}
 	
-    this.create = function() {
-
-        that.element = document.createElement('div');
-        that.element.className ='player';
-		parent.appendChild(that.element);
-        that.x=110;
-		that.y=500;
-       
-        that.element.style.top = that.y + "px";
-        that.element.style.left = that.x + "px";
-        that.element.style.backgroundColor = "yellogreen";
-		document.body.onkeydown=function(e)
-		{
-			console.log(e);
-			  var e = e || window.event;
-			  
-	if (e.keyCode == '37') {
-		if(that.x==220){
-        that.x=110;
-		pos=1;
-		}
-		else{
-		that.x=0;
-		pos=0;
-		}
-    }
-    else if (e.keyCode == '39') {
-    	if(that.x==110){
-        that.x=220;
-		pos=2;
-		}
-		else{
-		that.x=110;
-		pos=1;
-		}
-	}
-	else if(e.keyCode == '38')
-	{
-		if(that.y>=0)
-		that.y=that.y-4;
-	}
-	else if(e.keyCode == '40')
-	{
-		if(that.y<=500)
-		that.y=that.y+4;
-	}
-		that.element.style.top = that.y + "px";
-        that.element.style.left = that.x + "px";
-		test=that.y;
-		}
+	
+	function Player() {
+		this.x = 0;
+		this.y = 0;
+		this.element;
+		var that = this;
 		
-        //that.intervalId = setInterval(that.moveopponent, 20);
-    };
+		this.create = function() {
+	
+			that.element = document.createElement('div');
+			that.element.className ='player';
+			parent.appendChild(that.element);
+			that.x=110;
+			that.y=500;
+			that.element.style.top = that.y + "px";
+			that.element.style.left = that.x + "px";
+			that.element.style.backgroundColor = "yellogreen";
+			document.body.onkeydown=function(e)
+			{
+				 var e = e || window.event;
+				 if (e.keyCode == consts.left) {
+					if(that.x==consts.lane3Position){
+						that.x=consts.lane2Position;
+						pos=consts.lane2;
+					}
+					else{
+						that.x=consts.lane1Position;
+						pos=consts.lane1;
+					}
+				}
+				else if (e.keyCode == consts.right) {
+					if(that.x==consts.lane2Position){
+						that.x=consts.lane3Position;
+						pos=consts.lane3;
+					}
+					else{
+						that.x=consts.lane2Position;
+						pos=consts.lane2;
+					}
+				}
+				else if(e.keyCode == consts.top)
+				{
+					if(that.y>=0)
+					that.y=that.y-4;
+				}
+				else if(e.keyCode == consts.bottom)
+				{
+					if(that.y<=500)
+						that.y=that.y+4;
+				}
+				that.element.style.top = that.y + "px";
+				that.element.style.left = that.x + "px";
+				test=that.y;
+			}
+		};
+	}
+	
+	function exitGame(){
+		clearInterval(e);
+		var sc=document.createElement("div");
+		var newGame=document.createElement("div");
+		newGame.className="ng";
+		newGame.innerHTML="NEW GAME";
+		sc.className="score";
+		sc.innerHTML=currentScore;
+		parent.appendChild(newGame);
+		parent.appendChild(sc);
+		newGame.onclick=function(){
+		window.location="file:///C:/Users/Roshan/Desktop/Car%20Race/index.htm";
+		var t=window.location;
+		return  t;
+	}
 }
-
-function exitGame(){
-	clearInterval(e);
-	var sc=document.createElement("div");
-	var newGame=document.createElement("div");
-	newGame.className="ng";
-	newGame.innerHTML="NEW GAME";
-	sc.className="score";
-	sc.innerHTML=currentScore;
-	parent.appendChild(newGame);
-	parent.appendChild(sc);
-	newGame.onclick=function(){
-			window.location="file:///C:/Users/Roshan/Desktop/Car%20Race/index.htm";
-	var t=window.location;
-			return  t;
-		}
-	//alert("et");
-	 //window.location="file:///C:/Users/Roshan/Desktop/Car%20Race/index.htm";
-	 }
